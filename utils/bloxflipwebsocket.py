0688a8f78cb4cd7727b5bd1f3c043807476acea9
coxy57 <89717168+coxy57@users.noreply.github.com>
2023-12-02 18:11:28 -0500

    Update bloxflipwebsocket.py

--- utils/bloxflipwebsocket.py
+++ utils/bloxflipwebsocket.py
@@ -2,13 +2,13 @@ import websocket, time, threading
 
 
 class BaseWebsocket(websocket.WebSocketApp):
-    def __init__(self,auth):
+    def init(self,auth):
         self.auth = auth
         self.base_headers = {
-            'Origin': 'https://bloxflip.com',
+            'Origin': 'https://bloxflip.com/',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         }
-        super().__init__(url="wss://ws.bloxflip.com/socket.io/?EIO=3&transport=websocket", header=self.base_headers,
+        super().init(url="wss://ws.bloxflip.com/socket.io/?EIO=3&transport=websocket", header=self.base_headers,
                          on_message=lambda ws, msg: self.client_recv(ws, msg), on_open=lambda ws: self.open(ws),
                          on_close=lambda ws, close_status_code, close_msg: self.on_close(ws, close_status_code,
                                                                                          close_msg))
@@ -18,15 +18,12 @@ class BaseWebsocket(websocket.WebSocketApp):
             print('Server is being pinged!')
             ws.send('2')
         else:
-            pass
+           print(msg)
     def join_rain_data(self, token):
         try:
-            if self.sock.connected:
-                self.sock.send(f'42/chat,["enter-rain",{{"captchaToken":"{token};;64aZa0UMFbj3CVV7kO4UHuiT8mAdCJT;;scope"}}]')
-            else:
-                return "Not connected."
+            self.send(f'42/chat,["enter-rain",{{"captchaToken":"{token};;64aZa0UMFbj3CVV7kO4UHuiT8mAdCJT;;scope"}}]')
         except Exception as e:
-            self.sock.send(f'42/chat,["enter-rain",{{"captchaToken":"{token};;64aZa0UMFbj3CVV7kO4UHuiT8mAdCJT;;scope"}}]')
+            self.send(f'42/chat,["enter-rain",{{"captchaToken":"{token};;64aZa0UMFbj3CVV7kO4UHuiT8mAdCJT;;scope"}}]')
     def open(self, ws):
         ws.send('40/chat,')
         time.sleep(0.2)
